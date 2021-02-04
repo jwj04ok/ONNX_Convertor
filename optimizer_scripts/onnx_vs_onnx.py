Fix rotation direction.

optimizer_scripts/onnx_vs_onnx.py
@@ -27,7 +27,10 @@ def onnx_model_results(path_a, path_b, total_times=10, rotate_b=0):
         out_shape_a, out_shape_b = outputs_a[i].shape, outputs_b[i].shape
         out_shape_a = list(map(lambda x: x if type(x) == type(1) else 1, out_shape_a))
         out_shape_b = list(map(lambda x: x if type(x) == type(1) else 1, out_shape_b))
-        assert out_shape_a == out_shape_b, 'Output {} has unmatched shapes'.format(i)
+        if rotate_b % 180 == 0 or len(out_shape_a) != 4:
+            assert out_shape_a == out_shape_b, 'Output {} has unmatched shapes'.format(i)
+        else:
+            assert out_shape_a == [out_shape_b[0], out_shape_b[1], out_shape_b[3], out_shape_b[2]], 'Output {} has unmatched shapes'.format(i)
 
 
     # load onnx graph_a and graph_b, to find the initializer and inputs
@@ -90,7 +93,7 @@ def onnx_model_results(path_a, path_b, total_times=10, rotate_b=0):
         if rotate_b == 0:
             input_b = np.reshape(data, shape_b).astype(input_data_type_b)
         else:
-            input_b = np.rot90(input_a, (rotate_b//90), (2, 3))
+            input_b = np.rot90(input_a, (rotate_b//90), (3, 2))
 
         input_dict_a = {}
         input_dict_b = {}
@@ -107,7 +110,11 @@ def onnx_model_results(path_a, path_b, total_times=10, rotate_b=0):
         rb = session_b.run([], input_dict_b)
         for i in range(len(outputs_a)):
             results_a[i].append(ra[i])
-            results_b[i].append(rb[i])
+            np_rb = np.array(rb[i])
+            if rotate_b == 0 or len(np_rb.shape) != 4:
+                results_b[i].append(rb[i])
+            else:
+                results_b[i].append(np.rot90(np_rb, 4 - (rotate_b//90), (3, 2)).tolist())
         times += 1
 
     return results_a, results_b

optimizer_scripts/tools/soft_rotate.py
@@ -18,9 +18,9 @@ def conv_weight_rotate(g, const_node, counter_clock=False):
     # Transpose
     old_weight_np = helper.constant_to_numpy(const_node)
     if counter_clock:
-        new_weight_np = np.rot90(old_weight_np, 3, (2, 3))
+        new_weight_np = np.rot90(old_weight_np, 3, (3, 2))
     else:
-        new_weight_np = np.rot90(old_weight_np, 1, (2, 3))
+        new_weight_np = np.rot90(old_weight_np, 1, (3, 2))
     # Create new node
     new_node = helper.numpy_to_constant(const_node.output[0], new_weight_np)
     g.node.remove(const_node)
@@ -37,9 +37,11 @@ def soft_rotate(m, degree):
     # Transpose input
     for input_value in g.input:
         value_info_transpose(g, input_value)
-    last_shape = []
+    # Transpose output
+    for output_value in g.output:
+        value_info_transpose(g, output_value)
+    last_shape = None
     for node in g.node:
-        last_shape = None
         if node.op_type == 'Conv':
             # Transpose attributes
             dilations = helper.get_attribute_by_name(node, 'dilations')
@@ -148,9 +150,9 @@ def soft_rotate(m, degree):
             old_weight_np = helper.constant_to_numpy(const_node)
             old_weight_np = np.reshape(old_weight_np, (last_shape[0], last_shape[1], last_shape[2], last_shape[3], -1))
             if degree == 90:
-                new_weight_np = np.transpose(old_weight_np, 1, (2, 3))
+                new_weight_np = np.rot90(old_weight_np, 1, (3, 2))
             else:
-                new_weight_np = np.transpose(old_weight_np, 3, (2, 3))
+                new_weight_np = np.rot90(old_weight_np, 3, (3, 2))
             new_weight_np = np.reshape(new_weight_np, (-1, new_weight_np.shape[-1]))
             # Create new node
             new_node = helper.numpy_to_constant(const_node.output[0], new_weight_np)